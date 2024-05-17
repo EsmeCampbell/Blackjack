309580f9e857be9392938223f3e6e5275e61b245
EsmeCampbell <esme_campbell26@milton.edu>
2024-05-17 11:27:25 -0400

    05/21 HW

--- src/Card.java
+++ src/Card.java
@@ -8,12 +8,76 @@ public class Card {
     //name ->Ace, 2, 3, ... Jack, Queen, King
     public String Name;
 
-    public Card(String pSuit, int pValue, String pName){
-        Suit = pSuit;
-        Value = pValue;
-        Name = pName;
+    public Card(int pName, int pSuit){
+
+        if(pName == 0){
+            Value = 11;
+            Name = "Ace";
+        }
+        if(pName == 1){
+            Value = 2;
+            Name = "Two";
+        }
+        if(pName == 2){
+            Value = 3;
+            Name = "Three";
+        }
+        if(pName == 3){
+            Value = 4;
+            Name = "Four";
+        }
+        if(pName == 4){
+            Value = 5;
+            Name = "Five";
+        }
+        if(pName == 5){
+            Value = 6;
+            Name = "Six";
+        }
+        if(pName == 6){
+            Value = 7;
+            Name = "Seven";
+        }
+        if(pName == 7){
+            Value = 8;
+            Name = "Eight";
+        }
+        if(pName == 8){
+            Value = 9;
+            Name = "Nine";
+        }
+        if(pName == 9){
+            Value = 10;
+            Name = "Ten";
+        }
+        if(pName == 10){
+            Value = 10;
+            Name = "Jack";
+        }
+        if(pName == 11){
+            Value = 10;
+            Name = "Queen";
+        }
+        if(pName == 12){
+            Value = 10;
+            Name = "King";
+        }
+        if(pSuit == 0){
+            Suit = "Hearts";
+        }
+        if(pSuit == 1){
+            Suit = "Spades";
+        }
+        if(pSuit == 2){
+            Suit = "Hearts";
+        }
+        if(pSuit == 3){
+            Suit = "Clubs";
+        }
     }
 
-    public void display(){System.out.println("Card is the " + Name + " of " + Suit + ", valued at:" + Value);}
+
+
+    public void print(){System.out.println("The " + Name + " of " + Suit + " is worth " + Value + " points.");}
 
 }
--- src/Casino.java
+++ src/Casino.java
@@ -1,9 +1,32 @@
 public class Casino {
+
+    public Card Card;
+
+    public Card[] deck;
     public static void main(String[] args) {
         Casino x = new Casino();
     }
 
-    public Casino(){
+    public Casino() {
         System.out.println("Welcome to Esme's Casino!");
+
+        deck = new Card[52];
+
+        int door = 0;
+        for (int j = 0; j < 4; j++){
+            for (int i = 0; i < 13; i++) {
+                Card = new Card(i, j);
+                Card.print();
+                deck[door] = Card;
+                door ++;
+            }
+        }
+        printDeck();
+    }
+
+    public void printDeck() {
+        for (int i = 0; i < deck.length; i++) {
+            deck[i].print();
+        }
     }
 }
